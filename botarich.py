648c2393fcd349faf75aff4e7eb6161a2b0eecec
LIFEPLANT59 <kostalsh09@gmail.com>
2026-05-30 22:12:40 +0300

    feat: подключена локальная нейросеть Qwen3.5:4b
    - Добавлены файлы

--- botarich.py
+++ botarich.py
@@ -1,7 +1,6 @@
 import os
 from dotenv import load_dotenv
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
-from telegram.request import HTTPXRequest
 
 from keyboards import get_main_keyboard
 from handlers.commands import start, help_command, menu_command
@@ -14,20 +13,16 @@ TOKEN = os.getenv("BOT_TOKEN")
 if not TOKEN:
     raise ValueError("Токен не найден! Убедитесь, что файл .env содержит BOT_TOKEN")
 
-
-PROXY_URL = "http://127.0.0.1:9697"
-
 if __name__ == "__main__":
-    app = ApplicationBuilder().token(TOKEN).request(
-        HTTPXRequest(proxy_url=PROXY_URL)
-    ).get_updates_request(
-        HTTPXRequest(proxy_url=PROXY_URL)
-    ).build()
+   
+    app = ApplicationBuilder().token(TOKEN).build()
 
+    
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("help", help_command))
     app.add_handler(CommandHandler("menu", menu_command))
 
+   
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_task_flow))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_navigation))
 
--- handlers/__init__.py
+++
@@ -1,3 +0,0 @@
-from .task_solver import handle_task_flow
-from .commands import start, help_command, menu_command
-from .navigation import handle_navigation
